cd23f5b3976d378335cc9e03f3e941c03f29a092
eeeeeeeeeeeaa <2764635119@qq.com>
2025-05-19 16:40:53 +0800

    test

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include "todolist.h"
+#include <QApplication>
+#include"login.h"
+
+#include "main_window.h"
+#include"myregister.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+ //   MainWindow w;
+ //   w.show();
+
+    main_window m;
+    m.show();
+
+//    login l;
+//    l.show();
+
+//    myregister m;
+//    m.show();
+
+
+
+
+    return a.exec();
+}
