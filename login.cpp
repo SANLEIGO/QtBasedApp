cce016444dd50b6f58b751487e7ba7882339d21c
eeeeeeeeeeeaa <2764635119@qq.com>
2025-05-19 16:27:20 +0800

    test

---
+++ login.cpp
@@ -0,0 +1,59 @@
+#include "login.h"
+#include "ui_login.h"
+#include "databasemanager.h"
+
+login::login(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::login)
+{
+    ui->setupUi(this);
+}
+
+login::~login()
+{
+    delete ui;
+}
+
+void login::on_pushButton_3_clicked()
+{
+
+
+    QString account = ui->login_account->text();
+    QString password= ui->login_password->text();
+
+    QSqlDatabase& db = databaseManager::getDataBase();
+
+    if(db.open()){
+
+        QSqlQuery query;
+
+        QString sql = QString("select account,password from users");
+
+        bool sign = 0;
+
+        if(query.exec(sql)){
+            while(query.next()){
+                if(query.value(0).toString()==account && query.value(1).toString()==password){
+                    sign = 1;
+                    QMessageBox::information(this,"登录提示","登录成功");
+
+                }
+
+            }
+            if(!sign){
+                QMessageBox::information(this,"登录提示", "账号或者密码错误");
+            }
+
+        }
+
+        else{
+             qDebug()<<"服务器忙,无法提供查询服务";
+        }
+
+    }
+    else{
+        QMessageBox::information(this,"登录提示","服务器忙");
+    }
+
+}
+
